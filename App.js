92a2733e580b2238a413a8614d8d6e930539a4f0
joma20 <jomalynenriquez03@gmail.com>
2023-04-27 20:19:30 +0800

    Recipe.jsx

---
+++ App.js
@@ -0,0 +1,17 @@
+function MyButton() {
+    return (
+      <button>
+        Comment
+      </button>
+    );
+  }
+  
+  export default function MyApp() {
+    return (
+      <div>
+        <h1>Recipe Sharing Platform</h1>
+        <MyButton />
+      </div>
+    );
+  }
+  
